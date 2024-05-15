da3612bfcbdb78be9f65d1dc1c37c778d92bf426
maxhu08 <140214354+maxhu08@users.noreply.github.com>
2024-05-15 18:00:45 -0500

    fix tmux setup script

--- arch/tmux-install.sh
+++ arch/tmux-install.sh
@@ -8,8 +8,8 @@ exec_step 1 4 "installing tmux..." "sudo pacman -S tmux"
 # installing tpm
 exec_step 2 4 "installing tmux plugin manager (tpm)..." "git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm"
 
-# copy tmux conf
-exec_step 3 4 "copy custom tmux config..." "cp ../resources/tmux.conf "$HOME/.tmux.conf""
+# copy and source tmux conf
+exec_step 3 4 "copying and sourcing custom tmux config..." "cp ../resources/tmux.conf "$HOME/.tmux.conf";tmux source-file ~/.tmux.conf"
 
 # install tpm plugins
 exec_step 4 4 "installing tpm plugins..." "~/.tmux/plugins/tpm/bin/install_plugins"
