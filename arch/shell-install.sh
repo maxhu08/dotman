0374879124679903fb546cdfa35e930871dc66f5
maxhu08 <140214354+maxhu08@users.noreply.github.com>
2024-05-15 17:31:15 -0500

    add utils and organize way script are written

--- arch/shell-install.sh
+++ arch/shell-install.sh
@@ -1,45 +1,14 @@
 #!/bin/bash
 
-print_colored_text() {
-  local step="$1"
-  local total_steps="$2"
-  local text="$3"
-
-  case $step in
-    1)
-      color="\e[31m"  # Red
-      ;;
-    2)
-      color="\e[32m"  # Green
-      ;;
-    3)
-      color="\e[34m"  # Blue
-      ;;
-    *)
-      color="\e[0m"   # Default color
-      ;;
-  esac
-
-  echo -e "$color[$step/$total_steps] $text\e[0m"
-}
-
-print_ok_text() {
-  echo -e "\e[32m[OK] $1\e[0m"
-}
+source utils.sh
 
 # install fish
-print_colored_text 1 3 "installing fish & more"
-echo "> sudo pacman -S fish zoxide eza"
-sudo pacman -S fish zoxide
+exec_step 1 3 "installing fish & more..." "sudo pacman -S fish zoxide eza"
 
 # set fish as default shell
-print_colored_text 2 3 "setting fish as default shell..."
-echo "> chsh -s /usr/bin/fish"
-chsh -s /usr/bin/fish
+exec_step 2 3 "setting fish as default shell..." "chsh -s /usr/bin/fish"
 
 # copy fish config
-print_colored_text 3 3 "copying fish config into ~/.config/fish/config.fish"
-echo "> mkdir -p ~/.config/fish; cp ../resources/config.fish ~/.config/fish/config.fish"
-mkdir -p ~/.config/fish; cp ../resources/config.fish ~/.config/fish/config.fish
+exec_step 3 3 "copying custom fish config..." "mkdir -p ~/.config/fish; cp ../resources/config.fish ~/.config/fish/config.fish"
 
-print_ok_text "Fish shell installed successfully."
+script_ok "shell install script finished"
---
+++ arch/utils.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+exec_step() {
+  step=$1
+  total_steps=$2
+  text=$3
+  command_to_execute=$4
+
+  step_str="$step/$total_steps"
+  
+  # colors
+  black="\e[1;30m"
+  red="\e[1;31m"
+  green="\e[1;32m"
+  yellow="\e[1;33m"
+  blue="\e[1;34m"
+  magenta="\e[1;35m"
+  cyan="\e[1;36m"
+  white="\e[1;37m"
+  reset="\e[0m"
+  
+  # log info
+  formatted_text="${magenta}dotman ${blue}[${green}${step}${blue}/${green}${total_steps}${blue}] ${yellow}${text}${reset}"
+  echo -e "$formatted_text"
+  
+  # exec command
+  echo -e "${yellow}$ ${green}$command_to_execute${reset}\n"
+  eval "$command_to_execute"
+}
+
+# example
+# exec_step 1 5 "Running some command" "echo "test""
+
+script_ok() {
+  message=$1
+  
+  # colors
+  black="\e[1;30m"
+  red="\e[1;31m"
+  green="\e[1;32m"
+  yellow="\e[1;33m"
+  blue="\e[1;34m"
+  magenta="\e[1;35m"
+  cyan="\e[1;36m"
+  white="\e[1;37m"
+  reset="\e[0m"
+  
+  # log info
+  formatted_text="${magenta}dotman ${blue}[${green}OK${blue}] ${yellow}${message}${reset}"
+
+  echo -e "$formatted_text"
+}
+
+# example
+# script_ok "script is done"
