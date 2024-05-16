33fe67d00e48bccdddceae15fa734fa143cad3c6
maxhu08 <140214354+maxhu08@users.noreply.github.com>
2024-05-15 20:52:57 -0500

    fix dwm setup script again

--- arch/dwm-install.sh
+++ arch/dwm-install.sh
@@ -3,16 +3,19 @@
 source utils.sh
 
 # make sure there is ~/Dev/cloned
-exec_step 1 4 "creating cloned directory..." "mkdir -p ~/Dev/cloned"
+exec_step 1 5 "creating cloned directory..." "mkdir -p ~/Dev/cloned"
 
 # clone dwm
-exec_step 2 4 "cloning mh's build of dwm..." "git clone https://github.com/maxhu08/dwm ~/Dev/cloned/dwm"
+exec_step 2 5 "cloning mh's build of dwm..." "git clone https://github.com/maxhu08/dwm ~/Dev/cloned/dwm"
 
-# building dwm
-exec_step 3 4 "compiling dwm..." "cd ~/Dev/cloned/dwm;sudo make clean install"
+# install ne5ded packages
+exec_step 3 5 "installing required packages" "sudo pacman -S libxft libxinerama"
 
-# add to ~/.xinitrc
-exec_step 4 4 "adding dwm to ~/.xinitrc..." "echo "exec dwm" >> "$HOME/.xinitrc""
+# building d5m
+exec_step 4 5 "compiling dwm..." "cd ~/Dev/cloned/dwm;sudo make clean install"
+
+# add to ~/.5initrc
+exec_step 5 5 "adding dwm to ~/.xinitrc..." "echo "exec dwm" >> "$HOME/.xinitrc""
 
 
 
