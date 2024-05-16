c0995fcd02db7c69c9ed023f5d3c11da4b56a3e4
maxhu08 <140214354+maxhu08@users.noreply.github.com>
2024-05-16 17:50:57 -0500

    add alacritty setup script

---
+++ arch/alacritty-install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+source utils.sh
+
+script_start "alacritty-install script"
+
+# install alacritty
+exec_step 1 2 "installing alacritty..." "sudo pacman -S alacritty"
+
+# copy alacritty.toml
+exec_step 2 2 "copying custom alacritty.conf..." "mkdir -p ~/.config/alacritty;cp ../resources/alacritty.toml ~/.config/alacritty/alacritty.toml"
+
+
+
+script_ok "alacritty install script finished"
