dde7a252c7e6bdb81e3a8176b33c4559272fc6c1
maxhu08 <140214354+maxhu08@users.noreply.github.com>
2024-05-16 19:37:31 -0500

    update paru install script to include rust

--- arch/paru-install.sh
+++ arch/paru-install.sh
@@ -5,7 +5,7 @@ source utils/utils.sh
 script_start "paru-install script"
 
 # install required packages
-exec_step 1 3 "installing required packages..." "sudo pacman -S base-devel"
+exec_step 1 3 "installing required packages..." "sudo pacman -S base-devel rust"
 
 # cloning paru
 exec_step 2 3 "cloning paru..." "mkdir -p ~/Dev/cloned/paru;git clone https://aur.archlinux.org/paru ~/Dev/cloned/paru"
