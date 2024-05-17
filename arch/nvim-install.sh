a2b09aac95a576b659bc472cff7007a4c4d8eecf
maxhu08 <140214354+maxhu08@users.noreply.github.com>
2024-05-16 19:10:11 -0500

    add neovim setup script

---
+++ arch/nvim-install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+source utils.sh
+
+script_start "neovim-install script"
+
+# install neovim
+exec_step 1 2 "installing neovim & more..." "sudo pacman -S neovim ripgrep nodejs npm bun;bun i -g prettier biome eslint"
+
+# use neovim-zen config
+exec_step 2 2 "cloning mh's build of neovim (neovim-zen)..." "mkdir -p ~/.config/nvim;git clone https://github.com/maxhu08/neovim-zen ~/.config/nvim"
+
+
+
+script_ok "neovim install script finished"
