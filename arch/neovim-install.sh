20029b0b4899fd7fb58454d2d6b170d9b267bed9
maxhu08 <140214354+maxhu08@users.noreply.github.com>
2024-05-16 19:14:40 -0500

    fix neovim script again

--- arch/neovim-install.sh
+++ arch/neovim-install.sh
@@ -5,7 +5,7 @@ source utils.sh
 script_start "neovim-install script"
 
 # install neovim
-exec_step 1 2 "installing neovim & more..." "sudo pacman -S neovim ripgrep nodejs npm;curl -fsSL https://bun.sh/install | bash;bun i -g prettier biome eslint"
+exec_step 1 2 "installing neovim & required packages..." "sudo pacman -S neovim ripgrep nodejs npm unzip;curl -fsSL https://bun.sh/install | bash;bun i -g prettier biome eslint"
 
 # use neovim-zen config
 exec_step 2 2 "cloning mh's build of neovim (neovim-zen)..." "mkdir -p ~/.config/nvim;git clone https://github.com/maxhu08/neovim-zen ~/.config/nvim"
