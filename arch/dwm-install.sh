d3c5b53084cae5292b6e5d675f3535d7fc564871
maxhu08 <140214354+maxhu08@users.noreply.github.com>
2024-05-19 10:10:09 -0500

    fix typo

--- arch/dwm-install.sh
+++ arch/dwm-install.sh
@@ -13,10 +13,10 @@ exec_step 2 5 "cloning mh's build of dwm..." "git clone https://github.com/maxhu
 # install needed packages
 exec_step 3 5 "installing required packages" "sudo pacman -S --noconfirm libxft libxinerama ttf-jetbrains-mono ttf-jetbrains-mono-nerd dmenu"
 
-# building d5m
+# building dwm
 exec_step 4 5 "compiling dwm..." "cd ~/Dev/cloned/dwm;sudo make clean install"
 
-# add to ~/.5initrc
+# add to ~/.xinitrc
 exec_step 5 5 "adding dwm to ~/.xinitrc..." "echo 'exec dwm' > '$HOME/.xinitrc'"
 
 
