8e6ee744f075b3ab93ed283739f8454f3b11968c
maxhu08 <140214354+maxhu08@users.noreply.github.com>
2024-05-24 13:21:13 -0500

    fix picom setup script

--- arch/picom-install.sh
+++ arch/picom-install.sh
@@ -2,10 +2,10 @@
 
 source utils/utils.sh
 
-script_start "picom install script"
+script_start "picom install script [REQUIRES PARU]"
 
-# install picom
-exec_step 1 2 "installing picom..." "sudo pacman -S --noconfirm picom"
+# install picom-git (PICOM DOESN'T WORK)
+exec_step 1 2 "installing picom..." "paru -S --noconfirm picom-git"
 
 # copy picom config
 exec_step 2 2 "copying custom picom config..." "mkdir -p ~/.config/picom;cp ../resources/picom.conf ~/.config/picom/picom.conf"
