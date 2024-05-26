53cf39012aa5bfce34e2c1219eba87ef68232f9f
maxhu08 <140214354+maxhu08@users.noreply.github.com>
2024-05-26 10:40:14 -0500

    add install extra programs script

---
+++ arch/install-extras.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+source utils/utils.sh
+
+N="install-extras"
+S="1"
+
+script_start $N
+
+# install extra programs
+exec_step $N 1 $S "installing extra programs..." "paru -S --noconfirm feh nemo brave-bin discord visual-studio-code-bin nm-connection-editor"
+
+
+
+script_ok $N
