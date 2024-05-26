30639098fa489a7a71db7ac891ed369a0f0fa156
maxhu08 <140214354+maxhu08@users.noreply.github.com>
2024-05-26 10:37:48 -0500

    add copy scripts script

---
+++ arch/copy-scripts.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+source utils/utils.sh
+
+N="copy-scripts"
+S="2"
+
+script_start $N
+
+# install directory
+exec_step $N 1 $S "making scripts directory..." "mkdir -p ~/Dev/scripts"
+
+# copying scripts
+exec_step $N 2 $S "copying scripts..." "cp ../resources/scripts/ ~/Dev/scripts/"
+
+
+
+script_ok $N
